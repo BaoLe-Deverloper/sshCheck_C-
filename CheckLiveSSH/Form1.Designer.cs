06f2bd75815f3f30c754b6205a9cce11045564c4
Baole-Deverloper <baolehvpkkq@gmail.com>
2020-03-25 21:31:50 +0300

    25/3/2020

---
+++ CheckLiveSSH/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CheckLiveSSH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_load = new System.Windows.Forms.Button();
+            this.gv_data = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(629, 4);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 23);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "Browser...";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gv_data
+            // 
+            this.gv_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv_data.Location = new System.Drawing.Point(12, 83);
+            this.gv_data.Name = "gv_data";
+            this.gv_data.Size = new System.Drawing.Size(962, 427);
+            this.gv_data.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chọn file SSH:";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(103, 6);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(520, 20);
+            this.txt_url.TabIndex = 3;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(629, 54);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 4;
+            this.btn_start.Text = "Bắt đầu";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 586);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gv_data);
+            this.Controls.Add(this.btn_load);
+            this.Name = "Form1";
+            this.Text = "Tool check live SSH - http://laptrinhvb.net";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gv_data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.DataGridView gv_data;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ CheckLiveSSH/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckLiveSSH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        DataTable table;
+        public  DataTable Convert(string File, string TableName, string delimiter)
+        {
+            table = new DataTable();
+
+            table.Columns.Add("ipaddress", typeof(string));
+            table.Columns.Add("username", typeof(string));
+            table.Columns.Add("password", typeof(string));
+            table.Columns.Add("location", typeof(string));
+            table.Columns.Add("pos", typeof(string));
+            table.Columns.Add("city", typeof(string));
+            table.Columns.Add("status", typeof(string));
+
+            StreamReader s = new StreamReader(File);           
+                 
+            string AllData = s.ReadToEnd();
+            string[] stringSeparators = new string[] { "\r\n" };
+            string[] rows = AllData.Split(stringSeparators, StringSplitOptions.None);         
+            foreach (string r in rows)
+            {              
+                string[] items = r.Split(delimiter.ToCharArray());
+                table.Rows.Add(items);
+            }    
+            return table;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt) | *.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string url = ofd.FileName;
+                txt_url.Text = url;
+                DataTable dt = Convert(url, "test", "|");
+                gv_data.DataSource = dt;
+
+            }
+        }
+        int somay = 0;
+        private int _soMayChon;
+        private int _mayChamXong;
+        private int somayChamXong
+        {
+            set
+            {
+                lock (gv_data)
+                {
+                    _mayChamXong = value;
+                    if (_mayChamXong >= _soMayChon)
+                    {
+                        
+                        //progressBarControl1.BeginInvoke(new Action(() =>
+                        //{
+                        //    Double percent = 100;
+                        //    progressBarControl1.EditValue = percent;
+                        //}));
+                    }
+                    else
+                    {
+                        //progressBarControl1.BeginInvoke(new Action(() =>
+                        //{
+                        //    Double percent = Convert.ToDouble(_mayChamXong) / Convert.ToDouble(_soMayChon) * 100.0;
+                        //    progressBarControl1.EditValue = percent;
+                        //    Load_DuLieu();
+                        //}));
+                    }
+                }
+            }
+            get
+            {
+                lock (gv_data)
+                {
+                    return _mayChamXong;
+                }
+            }
+        }
+        private void TinhTrangKetNoi(int rowHandle, string msg)
+        {
+            gv_data.BeginInvoke((Action)(() =>
+            {
+                gv_data[6, rowHandle].Value = msg;
+            }));
+        }
+        private  void btn_start_Click(object sender, EventArgs e)
+        {
+            _soMayChon = table.Rows.Count;
+            somayChamXong = 0;
+            //int i = 0;
+
+            var ipList = new List<KeyValuePair<int, string>>();
+
+            for (var r = 0; r <= table.Rows.Count - 1; r++)
+            {
+                somay++;
+                              
+                ipList.Add(new KeyValuePair<int, string>(r, table.Rows[r]["ipaddress"].ToString()));
+               
+            }
+            var smallIpLists = SplitList(ipList, 5); //chia list chính thành nhiều list co nhieu nhat 5 ip 
+            // bạn debug mà xem list nó như thế nào nhé
+
+            if (somay > 0)
+            {
+                foreach (var _listIp in smallIpLists) //chay thread cho moi list
+                {
+                    new Thread(() => {
+
+                       foreach(var ipInfo in _listIp)
+                        {
+                           new Thread(async () =>
+                            {
+                                TinhTrangKetNoi(ipInfo.Key, "Đang kiểm tra...");
+                                int r = ipInfo.Key;
+                                string username = table.Rows[r]["username"].ToString();
+                                string password = table.Rows[r]["password"].ToString();
+                                string result = await new MySSH().checkSSH(ipInfo.Value, username, password, "google.com", 10);
+                                TinhTrangKetNoi(ipInfo.Key, result);
+                            }).Start();
+                        };
+                    }).Start();
+                }
+               
+             
+                    
+                
+            }
+        }
+        public static List<List<T>> SplitList<T>(IEnumerable<T> values, int groupSize, int? maxCount = null)
+        {
+            List<List<T>> result = new List<List<T>>();
+            // Quick and special scenario
+            if (values.Count() <= groupSize)
+            {
+                result.Add(values.ToList());
+            }
+            else
+            {
+                List<T> valueList = values.ToList();
+                int startIndex = 0;
+                int count = valueList.Count;
+                int elementCount = 0;
+
+                while (startIndex < count && (!maxCount.HasValue || (maxCount.HasValue && startIndex < maxCount)))
+                {
+                    elementCount = (startIndex + groupSize > count) ? count - startIndex : groupSize;
+                    result.Add(valueList.GetRange(startIndex, elementCount));
+                    startIndex += elementCount;
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
